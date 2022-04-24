a6da96a114c97f7647702bb5da7b0b454f94096f
mythic-hypercurve <67553039+mythic-hypercurve@users.noreply.github.com>
2022-04-24 18:18:07 +0100

    Added buttons to html, started refactor of playerPlay.

--- script.js
+++ script.js
@@ -24,77 +24,81 @@ function computerPlay() {
   console.log(`Computer Chooses ${value}`);
   return value;
 }
-
 //User is asked for an input
 
-function playerPlay() {
-  let valid = false;
-  let playerSelection;
-  while (valid === false) {
-    playerSelection = prompt('Rock, Paper, or Scissors?');
-    valid = inputValidation(playerSelection);
-  }
+//Listen for click event on RPS buttons
+const rockBtn = document.getElementById('rockBtn');
+rockBtn.addEventListener('click', playerPlay);
 
-  console.log(`Player Chooses ${playerSelection}`);
-  return playerSelection;
+const paperBtn = document.getElementById('paperBtn');
+paperBtn.addEventListener('click', playerPlay);
+
+const scissorsBtn = document.getElementById('scissorsBtn');
+scissorsBtn.addEventListener('click', playerPlay);
+
+function playerPlay(e) {
+  console.log(e.target.attributes.value.nodeValue);
+  // let playerSelection = ;
+  // console.log(`Player Chooses ${playerSelection}`);
+  // return playerSelection;
 }
 
 //Is the input valid?
 
-function inputValidation(value) {
-  let playerLowercase = value.toLowerCase();
-  console.log(playerLowercase);
-  if (!['rock', 'paper', 'scissors'].includes(playerLowercase)) {
-    value = 'Invalid Selection';
-    console.log(value);
-  }
-  //If not display error and ask again
-  if (value === 'Invalid Selection') {
-    alert(
-      "You didn't write Rock, Paper, or Scissors. Check your typing and try again!"
-    );
-    return false;
-  }
-  return true;
-}
+// function inputValidation(value) {
+//   let playerLowercase = value.toLowerCase();
+//   console.log(playerLowercase);
+//   if (!['rock', 'paper', 'scissors'].includes(playerLowercase)) {
+//     value = 'Invalid Selection';
+//     console.log(value);
+//   }
+//   //If not display error and ask again
+//   if (value === 'Invalid Selection') {
+//     alert(
+//       "You didn't write Rock, Paper, or Scissors. Check your typing and try again!"
+//     );
+//     return false;
+//   }
+//   return true;
+// }
 
 //Compare computer and user inputs
-function playRound(playerSelection, computerSelection) {
-  let player = playerSelection.toLowerCase();
-  let computer = computerSelection.toLowerCase();
-  if (player === computer) {
-    return `A tie! You both chose ${capitalise(player)}!`;
-  } else if (
-    (player === 'rock' && computer === 'paper') ||
-    (player === 'paper' && computer === 'scissors') ||
-    (player === 'scissors' && computer === 'rock')
-  ) {
-    computerScore++;
-    return `You Lose! ${capitalise(computer)} beats ${capitalise(player)}!`;
-  } else if (
-    (player === 'paper' && computer === 'rock') ||
-    (player === 'scissors') & (computer === 'paper') ||
-    (player === 'rock') & (computer === 'scissors')
-  ) {
-    playerScore++;
-    return `You Win! ${capitalise(player)} beats ${capitalise(computer)}`;
-  }
-}
+// function playRound(playerSelection, computerSelection) {
+//   let player = playerSelection.toLowerCase();
+//   let computer = computerSelection.toLowerCase();
+//   if (player === computer) {
+//     return `A tie! You both chose ${capitalise(player)}!`;
+//   } else if (
+//     (player === 'rock' && computer === 'paper') ||
+//     (player === 'paper' && computer === 'scissors') ||
+//     (player === 'scissors' && computer === 'rock')
+//   ) {
+//     computerScore++;
+//     return `You Lose! ${capitalise(computer)} beats ${capitalise(player)}!`;
+//   } else if (
+//     (player === 'paper' && computer === 'rock') ||
+//     (player === 'scissors') & (computer === 'paper') ||
+//     (player === 'rock') & (computer === 'scissors')
+//   ) {
+//     playerScore++;
+//     return `You Win! ${capitalise(player)} beats ${capitalise(computer)}`;
+//   }
+// }
 
-//Inform user of outcome
-function game() {
-  while (playerScore < 5 && computerScore < 5) {
-    let a = playerPlay();
-    let b = computerPlay();
+// //Inform user of outcome
+// function game() {
+//   // while (playerScore < 5 && computerScore < 5) {
+//   let a = playerPlay();
+//   let b = computerPlay();
 
-    console.log(playRound(a, b));
-    console.log({ playerScore, computerScore });
-  }
-  if (playerScore > computerScore) {
-    console.log('Congratulations. You Win!');
-  } else {
-    console.log('The Computer Wins! You Lose!');
-  }
-}
+//   console.log(playRound(a, b));
+//   console.log({ playerScore, computerScore });
+// }
+// if (playerScore > computerScore) {
+//   console.log('Congratulations. You Win!');
+// } else {
+//   console.log('The Computer Wins! You Lose!');
+// }
+// // }
 
-game();
+// game();
